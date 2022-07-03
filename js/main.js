1f5ae47e647d306eeb52f8b6f036d4725ddf7960
umarofz <webnajottalim@gmail.com>
2022-07-03 12:14:46 +0500

    add

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// FizzBuzz oyin
+
 const number = prompt('Kiriting')
 
 if (number % 3 === 0 && number % 5 === 0){
@@ -6,4 +8,14 @@ if (number % 3 === 0 && number % 5 === 0){
   alert('fizz')
 } else if (number % 5 === 0) {
   alert('buzz')
+}
+
+// Salomlashadigan robot
+
+const name = prompt('Ismingiz')
+
+if (name && !Number(name)) {
+  alert (`Assalomu alaykum ${name}`)
+} else {
+  alert('Ismingizni kiriting!!!')
 }
